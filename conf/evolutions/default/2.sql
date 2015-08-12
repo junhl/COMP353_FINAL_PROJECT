cd6b954bf7553fc63ed62ef5f3c4366e91d6e9e7
Jack <haoran.sun.1993@gmail.com>
2015-08-12 12:23:42 -0400

    Update 2.sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -52,11 +52,13 @@ insert into employee(name, unit_id, role_id) values ('A NURSE SUPERVISOR', 1, 4)
 insert into employee(name, unit_id, role_id) values ('A NURSE', 1, 5);
 insert into employee(name, unit_id, role_id) values ('A INTERN', 1, 6);
 
-insert into service (name,unit_id) values ('test1', 1);
-insert into service (name,unit_id) values ('test2', 1);
-insert into service (name,unit_id) values ('test3', 2);
-insert into service (name,unit_id) values ('test4', 3);
-insert into service (name,unit_id) values ('test4', 3);
+insert into service (name,unit_id) values ('TEST SERVICE 1 UNIT 1', 1);
+insert into service (name,unit_id) values ('TEST SERVICE 2 UNIT 1', 1);
+
+insert into service (name,unit_id) values ('TEST SERVICE 1 UNIT 2', 2);
+
+insert into service (name,unit_id) values ('TEST SERVICE 1 UNIT 3', 3);
+insert into service (name,unit_id) values ('TEST SERVICE 2 UNIT 3', 3);
 
 
 insert into patient (name,medicard_ID,hospitalcard_ID) values ('John Smith', 111, 123);
