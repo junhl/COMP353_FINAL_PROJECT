b199e1064f7c3cb1f3e4d9480937224b80ad664c
maxedoutstudio <max@maxedoutstudio.com>
2015-08-20 06:39:40 -0400

    Added Nurse, Nurse Supervisor Functionality. Everything except 2.sql is already merged with most up to date project version.

--- app/controllers/NurseShiftSupervisorController.java
+++ app/controllers/NurseShiftSupervisorController.java
@@ -2,12 +2,80 @@ package controllers;
 
 import play.mvc.*;
 import views.html.*;
+import play.data.*;
+
+import static play.data.Form.*;
+
+import models.*;
+
 
 public class NurseShiftSupervisorController extends Controller{
 	
-    public static Result index() {
-        return TODO;
+   public static Result index() {
+    	return ok(
+    			nurse_shift_supervisor_index.render()
+    			);
+    }	
+
+
+
+    //------------------------------- Nurses -------------------------------\\
+    public static Result nurse_index() {
+    	return nurse(0, "name", "asc", (long) 4); 
 
     }
+	
+	public static Result nurse(int page, String sortBy, String order, Long filter) {
+        return ok( //ok is to display
+        		nurse_shift_supervisor_nurses.render(Supervision.page(page, 10, sortBy, order, filter),sortBy, order, filter)
+				);
+    }
+
+
+     //------------------------------- Tasks -------------------------------\\
+
+    public static Result task_index(Long filter) {
+		return task(0, "id", "asc", filter);
+    }
+	
+	public static Result task(int page, String sortBy, String order, Long filter) {
+        return ok( //ok is to display
+        		nurse_shift_supervisor_tasks.render(Task.page(page, 10, sortBy, order, filter),sortBy, order, filter)
+				);
+    }
+
+	public static Result add_task(Long history_id){
+        Form<Task> taskForm = form(Task.class);
+        return ok(
+            nurse_shift_supervisor_add_task.render(history_id, taskForm)
+        );
+    }
+	
+	public static Result save_task() {
+        Form<Task> taskForm = form(Task.class).bindFromRequest();
+		
+        taskForm.get().save();
+        return nurse_index();
+    }
+	
+	public static Result edit_task(long id){ // this id is taskID to edit
+        Form<Task> taskForm = form(Task.class).fill(
+        		Task.find.byId(id)
+            );        
+        return ok(
+                nurse_shift_supervisor_edit_task.render(id, taskForm)
+            );
+    }
+	public static Result update_task(long id) {
+        Form<Task> taskForm = form(Task.class).bindFromRequest();
+        		
+		taskForm.get().update(id);
+        return nurse_index();
+    }
+	public static Result delete_task(long id) {
+        Task.find.ref(id).delete();
+        return nurse_index();
+    }
+
 
 }
